3c5a58f77932fa649a47b5fc22e87092eac7658d
Vaibhav Dhunde <vaibhavdhunde94@gmail.com>
2019-12-03 13:09:30 +0530

    Design auth screens

--- app/src/main/java/com/vaibhavdhunde/app/hackenglish/ui/login/AuthActivity.kt
+++ app/src/main/java/com/vaibhavdhunde/app/hackenglish/ui/login/AuthActivity.kt
@@ -1,12 +1,13 @@
-package com.vaibhavdhunde.app.hackenglish
+package com.vaibhavdhunde.app.hackenglish.ui.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.vaibhavdhunde.app.hackenglish.R
 
-class MainActivity : AppCompatActivity() {
+class AuthActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_auth)
     }
 }
